e00375eb845ad00d311031a19dc02831f37d6897
atomita <sleeping.cait.sith+gh@gmail.com>
2018-05-31 16:19:22 +0900

    feat: Add `xa-slice` attribute and its functoin

--- src/consts/attributeNames.ts
+++ src/consts/attributeNames.ts
@@ -1,3 +1,4 @@
 export const CONTEXT = 'xa-context'
 export const EXTRA = 'xa-extra'
 export const RECYCLE = 'xa-recycle'
+export const SLICE = 'xa-slice'
--- src/resolveNode.ts
+++ src/resolveNode.ts
@@ -1,13 +1,34 @@
 import { ATTRIBUTES, CHILDREN } from './consts/vdomAttributeNames'
-import { CONTEXT, EXTRA } from './consts/attributeNames'
+import { CONTEXT, EXTRA, SLICE } from './consts/attributeNames'
+import { deepGet } from './deepGet'
+import { deepSet } from './deepSet'
 
 export function resolveNode(node, parentNode) {
   const attributes = node && node[ATTRIBUTES]
 
   if (attributes) {
-    const context = attributes[CONTEXT]
+    let context = attributes[CONTEXT]
       || (parentNode && parentNode[ATTRIBUTES] && parentNode[ATTRIBUTES][CONTEXT])
       || {}
+
+    let slice = attributes[SLICE]
+    let sliced: any
+
+    if ('object' !== typeof slice) {
+      slice = [slice]
+    }
+    const path = slice[0]
+
+    if (path) {
+      sliced = deepGet(context, path)
+      if (!sliced) {
+        const defaultValue = slice[1] || {}
+        sliced = { ...defaultValue }
+        deepSet(context, path, sliced)
+      }
+      context = sliced
+    }
+
     const extra = {
       ...(attributes[EXTRA] || {}),
       ...(parentNode && parentNode[ATTRIBUTES] && parentNode[ATTRIBUTES][EXTRA] || {})
