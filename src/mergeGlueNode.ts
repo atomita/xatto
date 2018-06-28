815340afaaeb1e08b9f7c36519faeeb7e9be73ac
atomita <sleeping.cait.sith+gh@gmail.com>
2018-06-28 20:43:40 +0900

    fix: bug

--- src/mergeGlueNode.ts
+++ src/mergeGlueNode.ts
@@ -41,7 +41,9 @@ export function mergeGlueNode(vNode?, glueNode?) {
     for (i = 0; i < indexedPrevChildren.length; i++) {
       _prevChild = indexedPrevChildren[i]
       if (child[NAME] == _prevChild[NAME] && child[KEY] == _prevChild[KEY]) {
-        prevChild = _prevChild
+        if (UPDATE === _prevChild[LIFECYCLE] || CREATE === _prevChild[LIFECYCLE]) {
+          prevChild = _prevChild
+        }
         break
       }
     }
