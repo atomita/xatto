ab2a46f8b5379de86e824c41fffa3071ad663fd7
atomita <sleeping.cait.sith+gh@gmail.com>
2018-12-11 13:28:08 +0900

    Revert "feat: Memorize eventHandler"
    This reverts commit 24b610fad2013364c1a4441f34f6fc2c211a9d90.

--- src/currentOnlyEventHandler.ts
+++ src/currentOnlyEventHandler.ts
@@ -1,15 +1,7 @@
-const memorize = new WeakMap()
-
 export function currentOnlyEventHandler(eventHandler: Function) {
-  if (!memorize.has(eventHandler)) {
-    memorize.set(
-      eventHandler,
-      (context, props, event) => {
-        if (event.currentTarget === event.target) {
-          return eventHandler(context, props, event)
-        }
-      }
-    )
+  return (context, props, event) => {
+    if (event.currentTarget === event.target) {
+      return eventHandler(context, props, event)
+    }
   }
-  return memorize.get(eventHandler)
 }
