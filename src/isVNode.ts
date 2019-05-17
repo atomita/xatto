e301b9e48bbce835cb1e9741d85b6d3abba8bb9b
atomita <sleeping.cait.sith+gh@gmail.com>
2019-05-17 21:38:46 +0900

    fix: Improved strictness

--- src/isVNode.ts
+++ src/isVNode.ts
@@ -1,12 +1,23 @@
 import { CHILDREN, KEY, NAME, PROPS } from './consts/vNodeAttributeNames'
+import { VNode } from './VNode'
 
-export function isVNode (value) {
+const OBJECT = 'object'
+const STRING = 'string'
+
+const nameTypes = [STRING, 'function']
+
+export function isVNode (value): value is VNode {
   return (
     null != value &&
-    'object' === typeof value &&
+    OBJECT === typeof value &&
     PROPS in value &&
     CHILDREN in value &&
     KEY in value &&
-    NAME in value
+    NAME in value &&
+    OBJECT === typeof value[PROPS] &&
+    OBJECT === typeof value[CHILDREN] &&
+    'length' in value[CHILDREN] &&
+    (value[KEY] == null || STRING === typeof value[KEY]) &&
+    0 <= nameTypes.indexOf(typeof value[NAME])
   )
 }
